61a0149b674222d35b4e59c7568b6a2f73a874cb
zhouyuan63 <zhouyuan11@jd.com>
2022-09-07 19:38:46 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
   base: "/cherry/",
   plugins: [],
   themeConfig: {
-    version:"1.0.7", // 发布的版本号
+    version:"1.0.8", // 发布的版本号
     // 添加导航栏
     nav: [{
         text: "主页",
